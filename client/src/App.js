0619ccdba93e8a554b1909894b789203657bcc9e
Magdalena Pociecha <magdalena.pociecha11@gmail.com>
2019-01-16 17:39:45 +0100

    added navbar and movieposter

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,10 @@ import React, { Component } from 'react';
 import './App.css';
 import RegisterPage from './components/RegisterPage';
 import Login from './components/Login';
-<<<<<<< HEAD
 import Home from './components/Home';
-=======
 import Navbar from './components/Navbar';
 import MoviePoster from './components/MoviePoster';
 
->>>>>>> added Navbar and MoviePoster/MovieTitle
 
 class App extends Component {
   state = {
@@ -35,18 +32,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-<<<<<<< HEAD
       <Home/>
       <RegisterPage />
       <Login />
-        <p>{this.state.response}</p>
-=======
-      {/* <RegisterPage />
-      <Login /> */}
       <Navbar />
       <MoviePoster />
-      <p>{this.state.response}</p>
->>>>>>> added Navbar and MoviePoster/MovieTitle
+        <p>{this.state.response}</p>
       </div>
       
     );
