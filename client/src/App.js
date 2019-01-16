3697e7543fe7164b10ffc36b4395834f60cc441e
Magdalena Pociecha <magdalena.pociecha11@gmail.com>
2019-01-16 02:00:30 +0100

    added Navbar and MoviePoster/MovieTitle

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import RegisterPage from './components/RegisterPage';
 import Login from './components/Login';
+<<<<<<< HEAD
 import Home from './components/Home';
+=======
+import Navbar from './components/Navbar';
+import MoviePoster from './components/MoviePoster';
+
+>>>>>>> added Navbar and MoviePoster/MovieTitle
 
 class App extends Component {
   state = {
@@ -29,10 +35,18 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+<<<<<<< HEAD
       <Home/>
       <RegisterPage />
       <Login />
         <p>{this.state.response}</p>
+=======
+      {/* <RegisterPage />
+      <Login /> */}
+      <Navbar />
+      <MoviePoster />
+      <p>{this.state.response}</p>
+>>>>>>> added Navbar and MoviePoster/MovieTitle
       </div>
       
     );
---
+++ client/src/components/MoviePoster.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import axios from 'axios';
+import './style.css';
+
+//const api_key = "332654c71ccbb479020bcc047eaa43e8";
+
+class MoviePoster extends React.Component{
+    state = {
+        images: [],
+        title: []
+    }
+
+    componentDidMount(){
+        this.getMovie();
+    }
+    getMovie = ()=>{
+        axios.get('https://api.themoviedb.org/3/movie/upcoming?api_key=332654c71ccbb479020bcc047eaa43e8&language=en-US&page=1&region=pl')
+        .then(res=>{
+            this.setState({
+                images: res.data.results.slice(4,8)
+            })
+            
+        })  
+    }
+
+    render(){
+        const { images } = this.state;
+        const posters = images.map( image => {
+                return(
+                    <div className='poster-con'key={image.id}>
+                        <div className='poster-s-con' >
+                            <img className="poster responsive-img" src ={`http://image.tmdb.org/t/p/w300${image.poster_path}`} alt='img'/>
+                            <h5 className='movie-title'>{image.title}</h5>
+                        </div>
+                    </div>            
+                )
+            })
+
+        return(
+            <div className='movie-big-con white-text'>
+                <div className='movie-small-con'>
+                <div className='movie-con'>{posters}</div>
+                </div>
+            </div>
+        
+    )}
+
+};
+
+export default MoviePoster;
