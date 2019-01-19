1e4db845dabe309f97f82cacda9c2535e9d7676d
Jakub Zawiślak <jakubatub@gmail.com>
2019-01-19 21:44:35 +0100

    Added movie date, description

--- client/src/App.js
+++ client/src/App.js
@@ -5,17 +5,41 @@ import Login from './components/Login';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import MoviePoster from './components/MoviePoster';
+<<<<<<< HEAD
 import Summary from './components/Summary';
 import MovieRoom from './components/MovieRoom';
+=======
+import MovieDescription from './components/MovieDescription';
+import MovieDates from './components/MovieDates';
+import MoviePosterDescription from './components/MoviePosterDescription';
+>>>>>>> Added movie date, description
 
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.updateDat = this.updateData.bind(this)
+  }
   state = {
     response: '',
     post: '',
     responseToPost: '',
+    data: "",
+    flag: false,
+    flag2: false,
+    moreData: ""
   };
-
+ 
+  updateData = (data) => {
+    this.setState({data: data})
+    this.setState({flag: true})    
+  }
+  updateMoreData = (data) => {
+    this.setState({moreData: data})
+    this.setState({flag2: true})
+  }
+  
   componentDidMount() {
     this.callApi()
       .then(res => this.setState({
@@ -23,18 +47,26 @@ class App extends Component {
       }))
       .catch(err => console.log(err));
   }
+<<<<<<< HEAD
 
   callApi = async () => {
+=======
+ componentWillUpdate() {
+   
+ }
+   callApi = async () => {
+>>>>>>> Added movie date, description
      const response = await fetch('/api/hello');
      const body = await response.json();
      if (response.status !== 200) throw Error(body.message);
      return body;
    };
-
+ 
   render() {
     return (
       
       <div className="App">
+<<<<<<< HEAD
 
         <Login />
         <RegisterPage />
@@ -49,8 +81,25 @@ class App extends Component {
       
       
       
+=======
+      <Home/>
+      <RegisterPage />
+      <Login />
+      <Navbar />
+      <MoviePoster updateData = {this.updateData} updateMoreData = {this.updateMoreData} />
+      <div >
+      <Navbar />
+          <div className="row">
+            <MoviePosterDescription data = {this.state.data} />
+            <MovieDescription data = {this.state.data} flag = {this.state.flag} moreData = {this.state.moreData} />
+            <MovieDates />
+          </div>
+        </div>
+      </div>
+     
+>>>>>>> Added movie date, description
     );
   }
 }
-
+ 
 export default App;
--- client/src/components/MoviePoster.js
+++ client/src/components/MoviePoster.js
@@ -3,12 +3,27 @@ import axios from 'axios';
  
 import './style.css';
 
-//const api_key = "332654c71ccbb479020bcc047eaa43e8";
+const api_key = "896b160ed1c39f3b9471c1b72df80de9";
 
 class MoviePoster extends React.Component{
     state = {
         images: [],
         title: []
+    }
+
+    handleImgClick = (id) => {
+        fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}&language=en-US`)
+            .then(response => response.json())
+            .then(json => {
+                this.props.updateData(json);
+            });
+        fetch(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=${api_key}`)
+            .then(response => response.json())
+            .then(json => {
+                this.props.updateMoreData(json);
+            });
+        
+
     }
 
     componentDidMount(){
@@ -28,7 +43,7 @@ class MoviePoster extends React.Component{
         const { images } = this.state;
         const posters = images.map( image => {
                 return(
-                    <div className='poster-con'key={image.id}>
+                    <div onClick={() => this.handleImgClick(image.id)} className='poster-con'key={image.id}>
                         <div className='poster-s-con' >
                             <img className="poster responsive-img" src ={`http://image.tmdb.org/t/p/w300${image.poster_path}`} alt='img'/>
                             <h5 className='movie-title'>{image.title}</h5>
