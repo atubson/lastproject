4eed9107de500efa95008b303660adc30a750359
Magdalena Pociecha <magdalena.pociecha11@gmail.com>
2019-01-19 20:47:10 +0100

    added MovieRoom and seat booking

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,21 +3,23 @@ import './style.css';
 
 
 
-const Navbar= ()=>{
-    return(
-        <div className='my-navbar indigo darken-4 white-text'>
-            <div className='my-navbar-small'>
-                <div className=' nav-logo-container '>
-                    <img className=" img-nav-bar " src="/logo.png" alt="logo"/>
-                </div>
-                <div className='nav-right-con'>
-                    <div className='user-nav' >Username</div>
-                    <a href='/#' className='red hoverable btn waves-effect waves-light'>Logout</a>
-                </div>
-            </div>            
-        </div>
+class Navbar extends React.Component{
+    render(){
 
-    )
+        return(
+            <div className='my-navbar indigo darken-4 white-text'>
+                <div className='my-navbar-small'>
+                    <div className=' nav-logo-container '>
+                        <img className=" img-nav-bar " src="/logo.png" alt="logo"/>
+                    </div>
+                    <div className='nav-right-con'>
+                        <div className='user-nav' >Username</div>
+                        <a href='/' className='red hoverable btn waves-effect waves-light'>Logout</a>
+                    </div>
+                </div>            
+            </div>
+        )
+    }
 }
 
 
